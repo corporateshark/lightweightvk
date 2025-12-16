c2ce196e9e1360d11251b334b6c827aa4e80a988
Sergey Kosarevsky <sk@linderdaum.com>
2025-12-15 20:10:22 -0800

    Replaced `lvk::AccelStructGeomType` with `VkGeometryTypeKHR`

--- samples/RTX_002_AO.cpp
+++ samples/RTX_002_AO.cpp
@@ -763,7 +763,7 @@ bool initModel(const std::string& folderContentRoot) {
   const uint32_t totalPrimitiveCount = (uint32_t)indexData_.size() / 3;
   lvk::AccelStructDesc blasDesc{
       .type = lvk::AccelStructType_BLAS,
-      .geometryType = lvk::AccelStructGeomType_Triangles,
+      .geometryType = VK_GEOMETRY_TYPE_TRIANGLES_KHR,
       .vertexFormat = VK_FORMAT_R32G32B32_SFLOAT,
       .vertexBuffer = res.vb0_,
       .vertexStride = sizeof(VertexData),
@@ -772,7 +772,7 @@ bool initModel(const std::string& folderContentRoot) {
       .indexBuffer = res.ib0_,
       .transformBuffer = transformBuffer,
       .buildRange = {.primitiveCount = totalPrimitiveCount},
-      .buildFlags = lvk::AccelStructBuildFlagBits_PreferFastTrace,
+      .buildFlags = VK_BUILD_ACCELERATION_STRUCTURE_PREFER_FAST_TRACE_BIT_KHR,
       .debugName = "BLAS",
   };
   const lvk::AccelStructSizes blasSizes = ctx_->getAccelStructSizes(blasDesc);
@@ -824,10 +824,10 @@ bool initModel(const std::string& folderContentRoot) {
 
   res.TLAS = ctx_->createAccelerationStructure({
       .type = lvk::AccelStructType_TLAS,
-      .geometryType = lvk::AccelStructGeomType_Instances,
+      .geometryType = VK_GEOMETRY_TYPE_INSTANCES_KHR,
       .instancesBuffer = res.sbInstances_,
       .buildRange = {.primitiveCount = (uint32_t)instances.size()},
-      .buildFlags = lvk::AccelStructBuildFlagBits_PreferFastTrace,
+      .buildFlags = VK_BUILD_ACCELERATION_STRUCTURE_PREFER_FAST_TRACE_BIT_KHR,
   });
 
   return true;
