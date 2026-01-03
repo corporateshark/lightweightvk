5a4f90d1b4dd6b6bc0c3cddfeb5f817d3b412016
Sergey Kosarevsky <sk@linderdaum.com>
2026-01-03 01:52:24 -0800

    Enabled optional `VkPhysicalDeviceFeatures::independentBlend`

--- lvk/vulkan/VulkanClasses.cpp
+++ lvk/vulkan/VulkanClasses.cpp
@@ -6679,6 +6679,7 @@ lvk::Result lvk::VulkanContext::initContext(const HWDeviceDesc& desc) {
   }
 
   VkPhysicalDeviceFeatures deviceFeatures10 = {
+      .independentBlend = vkFeatures10_.features.independentBlend, // enable if supported
       .geometryShader = vkFeatures10_.features.geometryShader, // enable if supported
       .tessellationShader = vkFeatures10_.features.tessellationShader, // enable if supported
       .sampleRateShading = VK_TRUE,
