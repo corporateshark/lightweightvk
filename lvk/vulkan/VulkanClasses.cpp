f20e948a26fce96c6b22a30d8e2cee222acdf8f6
Sergey Kosarevsky <sk@linderdaum.com>
2026-01-07 20:10:04 -0800

    Removed unused code

--- lvk/vulkan/VulkanClasses.cpp
+++ lvk/vulkan/VulkanClasses.cpp
@@ -619,22 +619,6 @@ void transitionToColorAttachment(VkCommandBuffer buffer, lvk::VulkanImage* color
                              VkImageSubresourceRange{VK_IMAGE_ASPECT_COLOR_BIT, 0, VK_REMAINING_MIP_LEVELS, 0, VK_REMAINING_ARRAY_LAYERS});
 }
 
-bool isDepthOrStencilVkFormat(VkFormat format) {
-  switch (format) {
-  case VK_FORMAT_D16_UNORM:
-  case VK_FORMAT_X8_D24_UNORM_PACK32:
-  case VK_FORMAT_D32_SFLOAT:
-  case VK_FORMAT_S8_UINT:
-  case VK_FORMAT_D16_UNORM_S8_UINT:
-  case VK_FORMAT_D24_UNORM_S8_UINT:
-  case VK_FORMAT_D32_SFLOAT_S8_UINT:
-    return true;
-  default:
-    return false;
-  }
-  return false;
-}
-
 VkSurfaceFormatKHR chooseSwapSurfaceFormat(const std::vector<VkSurfaceFormatKHR>& formats, lvk::ColorSpace colorSpace) {
   LVK_ASSERT(!formats.empty());
 
