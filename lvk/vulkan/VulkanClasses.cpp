a40c4707229dcf9586b57664f3aebd84c639351e
Sergey Kosarevsky <sk@linderdaum.com>
2026-04-03 02:26:44 -0700

    Enabled optional feature `VkPhysicalDeviceFeatures::shaderStorageImageMultisample`

--- lvk/vulkan/VulkanClasses.cpp
+++ lvk/vulkan/VulkanClasses.cpp
@@ -7068,6 +7068,7 @@ lvk::Result lvk::VulkanContext::initContext(const HWDeviceDesc& desc) {
       .vertexPipelineStoresAndAtomics = vkFeatures10_.features.vertexPipelineStoresAndAtomics, // enable if supported
       .fragmentStoresAndAtomics = VK_TRUE,
       .shaderImageGatherExtended = VK_TRUE,
+      .shaderStorageImageMultisample = vkFeatures10_.features.shaderStorageImageMultisample,
       .shaderInt64 = vkFeatures10_.features.shaderInt64, // enable if supported
       .shaderInt16 = vkFeatures10_.features.shaderInt16, // enable if supported
   };
