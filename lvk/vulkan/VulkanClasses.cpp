a0097198276b034828000faa6c63dbab237b3194
Sergey Kosarevsky <sk@linderdaum.com>
2026-04-08 01:52:34 -0700

    Enabled `VkPhysicalDeviceFeatures::depthClamp`

--- lvk/vulkan/VulkanClasses.cpp
+++ lvk/vulkan/VulkanClasses.cpp
@@ -7061,6 +7061,7 @@ lvk::Result lvk::VulkanContext::initContext(const HWDeviceDesc& desc) {
       .sampleRateShading = vkFeatures10_.features.sampleRateShading, // enable if supported
       .multiDrawIndirect = VK_TRUE,
       .drawIndirectFirstInstance = VK_TRUE,
+      .depthClamp = VK_TRUE,
       .depthBiasClamp = vkFeatures10_.features.depthBiasClamp, // enable if supported,
       .fillModeNonSolid = vkFeatures10_.features.fillModeNonSolid, // enable if supported
       .samplerAnisotropy = VK_TRUE,
