61686125232e3d465b1fa0db3f6e6982dce73690
Sergey Kosarevsky <sk@linderdaum.com>
2026-05-22 23:06:18 -0700

    Added missing copyright headers
    Aligned with https://github.com/facebook/lightweightvk/commit/c1dbbb6bcf87e09d42ea149552b741acd9871a77

--- lvk/Pool.h
+++ lvk/Pool.h
@@ -1,4 +1,13 @@
-﻿#pragma once
+/*
+ * LightweightVK
+ *
+ * Copyright (c) 2023-2026 Sergey Kosarevsky and contributors.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#pragma once
 
 #include <assert.h>
 #include <cstdint>
