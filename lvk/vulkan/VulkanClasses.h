79996c0dc0dec677a6f2c5ac77c5da503b44d79d
Sergey Kosarevsky <sk@linderdaum.com>
2026-09-13 20:49:04 -0700

    Patch single descriptor slots instead of rebuilding the whole set for acceleration structures

--- lvk/vulkan/VulkanClasses.h
+++ lvk/vulkan/VulkanClasses.h
@@ -742,6 +742,7 @@ class VulkanContext final : public IContext {
   // return `false` when the caller has to fall back to a full rebuild via `awaitingCreation_`
   [[nodiscard]] bool writeTextureDescriptor(uint32_t index);
   [[nodiscard]] bool writeSamplerDescriptor(uint32_t index);
+  [[nodiscard]] bool writeAccelStructDescriptor(uint32_t index);
   void bindDefaultDescriptorSets(VkCommandBuffer cmdBuf, VkPipelineBindPoint bindPoint, VkPipelineLayout layout) const;
 
   [[nodiscard]] uint32_t getMaxStorageBufferRange() const override;
@@ -789,6 +790,7 @@ class VulkanContext final : public IContext {
   void getBuildInfoTLAS(const AccelStructDesc& desc,
                         VkAccelerationStructureGeometryKHR& outGeometry,
                         VkAccelerationStructureBuildSizesInfoKHR& outSizesInfo) const;
+  void createDummyTLAS();
 
  private:
   friend class lvk::VulkanSwapchain;
@@ -916,6 +918,9 @@ class VulkanContext final : public IContext {
   std::vector<const char*> enabledDeviceExtensionNames_;
 
   TextureHandle dummyTexture_;
+  // BLAS and empty slots of `kBinding_AccelerationStructures` need a valid TLAS too
+  BufferHandle dummyTLASBuffer_;
+  VkAccelerationStructureKHR dummyTLAS_ = VK_NULL_HANDLE;
 
   ldr::Pool<lvk::ShaderModule, lvk::ShaderModuleState> shaderModulesPool_;
   ldr::Pool<lvk::RenderPipeline, lvk::RenderPipelineState> renderPipelinesPool_;
